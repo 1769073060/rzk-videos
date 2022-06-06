cadbd8bb8e27ab19822bcc23d34952d01d67f4f1
Rzk <1769073060@qq.com>
2022-06-07 04:06:12 +0800

    update:更新依赖

---
+++ rzk-videos-common/src/main/java/com/rzk/utils/GetVideoPicture.java
@@ -0,0 +1,10 @@
+package com.rzk.utils;/**
+* @PackageName : com.rzk.utils
+* @FileName : GetVideoPicture
+* @Description : 
+* @Author : rzk
+* @CreateTime : 5/6/2022 上午2:06
+* @Version : v1.0
+*/
+public class GetVideoPicture {
+}
