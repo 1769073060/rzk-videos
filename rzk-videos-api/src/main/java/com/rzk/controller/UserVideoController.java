3676c9ec868f411c2ff5fa4722969c1abffc67a4
Rzk <1769073060@qq.com>
2022-06-10 02:44:52 +0800

    update:优化上传视频源

--- rzk-videos-api/src/main/java/com/rzk/controller/UserVideoController.java
+++ rzk-videos-api/src/main/java/com/rzk/controller/UserVideoController.java
@@ -1,5 +1,7 @@
 package com.rzk.controller;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
 import com.rzk.pojo.Bgm;
 import com.rzk.pojo.Comments;
 import com.rzk.pojo.Videos;
@@ -7,27 +9,32 @@ import com.rzk.service.BgmService;
 import com.rzk.service.UsersService;
 import com.rzk.service.VideosService;
 import com.rzk.utils.*;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.*;
 import io.swagger.annotations.*;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
-import java.util.UUID;
+import java.io.*;
+import java.util.*;
 
 @RestController
 @Api(value = "用户上传video有关业务的接口", tags = {"用户上传video业务controller"})
 @RequestMapping("/userVideo")
 public class UserVideoController extends BasicController {
+    private Logger logger = LoggerFactory.getLogger(UserVideoController.class);
 
     @Autowired
     private VideosService videosService;
@@ -35,9 +42,120 @@ public class UserVideoController extends BasicController {
     private UsersService usersService;
     @Autowired
     private FastDFSUtil fastDFSUtil;
+    @Value("${rzk.url}")
+    private String rzkUrl;
+    @Value(value = "${minio.endpoint}")
+    private String endpoint;
+
+    @Value(value = "${minio.accessKey}")
+    private String accessKey;
+
+    @Value(value = "${minio.secretKey}")
+    private String secretKey;
+
+    @Value("${minio.bucketName}")
+    private String bucketName;
 
     @Autowired
     private BgmService bgmService;
+    @Autowired
+    private MinioUtils minioUtils;
+
+    @PostMapping("/upload")
+    public Object upload(MultipartFile file) {
+
+        List<String> upload = minioUtils.upload(new MultipartFile[]{file});
+
+        return endpoint + "/" + bucketName + "/" + upload.get(0);
+    }
+
+    @PostMapping("/uploads")
+    public Object uploads(String address) throws Exception {
+        //获取视频的第一帧图片输出流
+        InputStream first = MinioUtils.randomGrabberFFmpegImage(address);
+        //获取文件名
+        String fileName = address.substring(address.lastIndexOf("/"), address.lastIndexOf(".")).concat(".jpg");
+        //将流转化为multipartFile
+        MultipartFile multipartFile = new MockMultipartFile("file", fileName, "image/jpg", first);
+
+        String upload = minioUtils.upload(multipartFile);
+
+        return endpoint + "/" + bucketName + "/" + upload;
+    }
+
+    /**
+     * 文件创建
+     * 如果文件是视频类型的，那么将文件截图，并将截图和视频上传到MinIO存储
+     * 如果文件是图片类型的，那么将图片上传到MinIo存储
+     */
+//    @Override
+//    @SneakyThrows
+    @ApiOperation(value = "Minio文件上传", notes = "Minio上传视频的接口")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "type", value = "文件类型", required = true, dataType = "String", paramType = "form"))
+    @PostMapping(value = "/minioFileUpload", headers = "content-type=multipart/form-data")
+    public HashMap minioFileUpload(MultipartFile file, String type) throws Exception {
+        HashMap map = new HashMap();
+
+        String date = DateUtil.formatDate(new Date());
+        String filename = DateUtil.currentSeconds() + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        if ("0".equals(type)) {
+            String imgName = "img" + "/" + date + "/" + filename;
+            minIoClientUpload(file.getInputStream(), imgName);
+            String videoUrl = endpoint + "/" + bucketName + "/" + imgName;
+            //获取视频的第一帧图片输出流
+            InputStream first = MinioUtils.randomGrabberFFmpegImage(videoUrl);
+            //获取文件名
+            String fileName = videoUrl.substring(videoUrl.lastIndexOf("/"), videoUrl.lastIndexOf(".")).concat(".jpg");
+            //将流转化为multipartFile
+            MultipartFile multipartFile = new MockMultipartFile("file", fileName, "image/jpg", first);
+
+            String pictureName = minioUtils.upload(multipartFile);
+            String pictureUrl = endpoint + "/" + bucketName + "/" + pictureName;
+
+            map.put("videoUrl", videoUrl);
+            map.put("pictureUrl", pictureUrl);
+            return map;
+        }
+
+        String videoType = file.getContentType();
+        // 文件存储的目录结构
+
+        String videoName = videoType + "/" + date + "/" + filename;
+
+        String imgPath = this.ffmpegGetScreenshot(file);
+        String imgPathName = DateUtil.currentSeconds() + imgPath.substring(imgPath.lastIndexOf("."));
+        BufferedInputStream imgInputStream = FileUtil.getInputStream(imgPath);
+
+        String imgName = "img" + "/" + date + "/" + imgPathName;
+
+        minIoClientUpload(imgInputStream, imgName);
+        minIoClientUpload(file.getInputStream(), videoName);
+        logger.info("图片文件上传成功!");
+        // 存储文件
+        logger.info("视频文件上传成功!");
+        String videoPath = endpoint + "/" + bucketName + "/" + videoName;
+        String imgPathOne = endpoint + "/" + bucketName + "/" + imgName;
+
+        map.put("videoPath", videoPath);
+        map.put("imgPath", imgPathOne);
+        //   logger.info("上传发生错误: {}！", e.getMessage());
+
+        return map;
+    }
+
+    public String ffmpegGetScreenshot(MultipartFile file) throws IOException {
+        File tofile = new File(file.getOriginalFilename());
+        FileUtils.copyInputStreamToFile(file.getInputStream(), tofile);
+        String absolutePath = tofile.getAbsoluteFile().getAbsolutePath();
+        Map<String, Object> screenshot = VideoUtils.getScreenshot(absolutePath);
+        String imgPath = (String) screenshot.get("imgPath");
+        return imgPath;
+    }
+
+
+
+
 
     @ApiOperation(value = "用户上传视频", notes = "用户上传视频的接口")
     @ApiImplicitParams({
@@ -53,134 +171,101 @@ public class UserVideoController extends BasicController {
         if (StringUtils.isBlank(userId)) {
             return JSONResult.errorMsg("用户id不能为空");
         }
-        //视频上传路径
-        //String fileDownloadPath = "E:\\lnsf_mod_dev";
-        //视频保存路径
+
+        //截取视频名
+        String originalFilename = video.getOriginalFilename();
+        int indexOf = originalFilename.lastIndexOf(".mp4");
+        String originalFileName = originalFilename.substring(0, indexOf);
 
 
         String videosid = null;
         String uploadFile = null;
+        String videoUrl = null;
+        String date = null;
         try {
             if (video != null) {
 
-                //String videoName = video.getOriginalFilename();
                 String videoName = UUID.randomUUID().toString() + ".mp4";
-                String videoName1 = UUID.randomUUID().toString() + ".mp4";
-                //String coverNamePrefix = videoName.split("\\.")[0];
-                String coverNamePrefix = UUID.randomUUID().toString();
-                //String coverNamePrefix = videoName;
                 if (StringUtils.isNotBlank(videoName)) {
-                    uploadFile = fastDFSUtil.uploadFile(video);
-
+                    date = DateUtil.formatDate(new Date());
+                    String filename = DateUtil.currentSeconds() + video.getOriginalFilename().substring(video.getOriginalFilename().lastIndexOf("."));
+                    String imgName = userId + "/" + "video" + "/" + date + "/" + originalFileName + "/" + filename;
+                    minIoClientUpload(video.getInputStream(), imgName);
+                    videoUrl = "/" + bucketName + "/" + imgName;
+                    logger.info("视频地址{}" + videoUrl);
 
                 }
             } else {
+                //后续可做处理如果视频上传成功后, 后续操作失败就把上传的视频删除
                 return JSONResult.errorMsg("上传功能出错");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //判断bgm是否为空，不为空则合成视频
-        if (StringUtils.isNotBlank(bgmId)) {
-            //删除原视频水印
-            //18.12.02新版本创建方式
-		/*	CommandManager manager=new CommandManagerImpl(1);
-			String ids =manager.start("delleft", CommandBuidlerFactory.createBuidler()
-					.add("ffmpeg").add("-i",videosFinalLocal)
-					.add("-filter_complex","\"delogo=x=10:y=10:w=250:h=100:show=0\"")
-					.add("-y",videosFinalLocal1));
-			Thread.currentThread().sleep(15000);
-			manager.stop("delleft");//停止*/
+        //获取视频的第一帧图片输出流
+        InputStream first = MinioUtils.randomGrabberFFmpegImage(endpoint + videoUrl);
+        //获取文件名
+        String fileJpgName = videoUrl.substring(videoUrl.lastIndexOf("/"), videoUrl.lastIndexOf(".")).concat(".jpg");
+        //根据用户名  日期 文件名 生成截图地址
+        String fileName = userId + "/" + "video" + "/" + date + "/" + originalFileName + fileJpgName;
+        //将流转化为multipartFile
+        MultipartFile multipartFile = new MockMultipartFile("file", fileName, "image/jpg", first);
+        //上传截图
+        String pictureName = minioUtils.upload(multipartFile);
+        String pictureUrl = "/" + bucketName + "/" + pictureName;
+        //视频截图工具
 
+        //保存视频信息到mysql
+        Videos videos = new Videos();
+        videos.setAudioId(bgmId);
+        videos.setUserId(userId);
+        videos.setVideoSeconds((float) videoSeconds);
+        videos.setVideoHeight(videoHeight);
+        videos.setVideoWidth(videoWidth);
+        videos.setVideoDesc(desc);
+        videos.setVideoPath(videoUrl);
+        videos.setCoverPath(pictureUrl);
+        videos.setStatus(UserVideoStatusEnum.BAN.value);
+        videos.setCreateTime(new Date());
+        videosid = videosService.insertVideos(videos);
 
-/*			int width =videoWidth-260;
-			int height = videoHeight -110;
-			manager.start("delright", CommandBuidlerFactory.createBuidler()
-					.add("ffmpeg").add("-i",videosFinalLocal1)
-					.add("-filter_complex","\"delogo=x=" + width + ":y=" + height + ":w=250:h=100:show=0\"")
-					.add("-y",videosFinalLocal));
-			Thread.currentThread().sleep(15000);
-			manager.stop("delright");//停止*/
-
-            //用于销毁保活线程等
-            //manager.destory();
-
-
-            String finalVideosPath = uploadFile;
-            //音频分离重组
-//			DelAudio delAudio = new DelAudio(FFMPEG_VIDEOS);
-//			Bgm bgm = bgmService.queryBgmId(bgmId);
-//			String bgmInputPath = FILE_DOWNLOAD_PATH + bgm.getPath();
-//			MergeVideoBgm tools = new MergeVideoBgm(FFMPEG_VIDEOS);
-//			//String	fileVideoFinalPath =videosFinalLocal;
-//				delAudio.convertor(videosFinalLocal, videosFinalLocal1);
-//				tools.convertor(videosFinalLocal1,bgmInputPath,
-//						videoSeconds, finalVideosPath);
-//			System.out.println("videosFinalLocal"+videosFinalLocal);
-//			System.out.println("videosFinalLocal1"+videosFinalLocal1);
-            System.out.println("finalVideosPath" + finalVideosPath);
-            //视频截图工具
-            FetchVideoCover videoCover = new FetchVideoCover(FFMPEG_VIDEOS);
-
-            //videoCover.getCover(finalVideosPath,FILE_DOWNLOAD_PATH+filecoverPath);
-            //保存视频信息到mysql
-            Videos videos = new Videos();
-            videos.setAudioId(bgmId);
-            videos.setUserId(userId);
-            videos.setVideoSeconds((float) videoSeconds);
-            videos.setVideoHeight(videoHeight);
-            videos.setVideoWidth(videoWidth);
-            videos.setVideoDesc(desc);
-            videos.setVideoPath(finalVideosPath);
-            videos.setCoverPath(finalVideosPath);
-            videos.setStatus(UserVideoStatusEnum.BAN.value);
-            videos.setCreateTime(new Date());
-            videosid = videosService.insertVideos(videos);
-        } else {
-
-            String finalVideosPath = uploadFile;
-            //删除原视频水印
-
-            //18.12.02新版本创建方式
-		/*	CommandManager manager=new CommandManagerImpl(1);
-			manager.start("delleft", CommandBuidlerFactory.createBuidler()
-					.add("ffmpeg").add("-i",videosFinalLocal)
-					.add("-filter_complex","\"delogo=x=10:y=10:w=250:h=100:show=0\"")
-					.add("-y",videosFinalLocal1));
-			Thread.currentThread().sleep(15000);
-			manager.stop("delleft");//停止*/
-
-/*
-			int width =videoWidth-260;
-			int height = videoHeight -110;
-			manager.start("delright", CommandBuidlerFactory.createBuidler()
-					.add("ffmpeg").add("-i",videosFinalLocal1)
-					.add("-filter_complex","\"delogo=x=" + width + ":y=" + height + ":w=250:h=100:show=0\"")
-					.add("-y",finalVideosPath));
-			Thread.currentThread().sleep(15000);
-			manager.stop("delright");//停止
-
-			//用于销毁保活线程等
-			manager.destory();*/
-
-            //视频截图工具
-
-            //保存视频信息到mysql
-            Videos videos = new Videos();
-            videos.setAudioId(bgmId);
-            videos.setUserId(userId);
-            videos.setVideoSeconds((float) videoSeconds);
-            videos.setVideoHeight(videoHeight);
-            videos.setVideoWidth(videoWidth);
-            videos.setVideoDesc(desc);
-            videos.setVideoPath(finalVideosPath);
-            videos.setCoverPath(finalVideosPath);
-            videos.setStatus(UserVideoStatusEnum.BAN.value);
-            videos.setCreateTime(new Date());
-            videosid = videosService.insertVideos(videos);
-        }
 
         return JSONResult.ok(videosid);
+    }
+
+    /**
+     * 上传
+     * @param imgInputStream
+     * @param objectName
+     * @throws Exception
+     */
+    public void minIoClientUpload(InputStream imgInputStream, String objectName) throws Exception {
+        //创建头部信息
+        Map<String, String> headers = new HashMap<>(10);
+        //添加自定义内容类型
+        headers.put("Content-Type", "application/octet-stream");
+        //添加存储类
+        headers.put("X-Amz-Storage-Class", "REDUCED_REDUNDANCY");
+        //添加自定义/用户元数据
+        Map<String, String> userMetadata = new HashMap<>(10);
+        userMetadata.put("My-Project", "Project One");
+
+
+        MinioClient minioClient =
+                MinioClient.builder()
+                        .endpoint(endpoint)
+                        .credentials(accessKey, secretKey)
+                        .build();
+
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .stream(imgInputStream, imgInputStream.available(), -1)
+                        .userMetadata(userMetadata)
+                        .build());
+        imgInputStream.close();
+
     }
 
 
